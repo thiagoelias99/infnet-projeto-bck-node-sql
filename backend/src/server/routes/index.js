4c2f0d0642974931b1317fc5c75e7d355a75dbb6
Thiago Elias <thiagoelias99@gmail.com>
2023-06-03 10:58:27 -0300

    adiciona rotas para alunos

--- backend/src/server/routes/index.js
+++ backend/src/server/routes/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const EmpregadosRoutes = require("./empregados")
-const DepartamentosRoutes = require("./departamentos")
+const AlunosRoutes = require("./alunos")
 const Route404 = require("./404")
 const WelcomeRoute = require("./welcome")
 
 router.use(WelcomeRoute)
-router.use(EmpregadosRoutes);
-router.use(DepartamentosRoutes);
+router.use(AlunosRoutes);
+// router.use(EmpregadosRoutes);
+// router.use(DepartamentosRoutes);
 router.use(Route404);
 
 module.exports = router;
