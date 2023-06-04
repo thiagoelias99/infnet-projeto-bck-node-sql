e8347d926b283b9a6f582f197e0839688bd33db4
Thiago Elias <thiagoelias99@gmail.com>
2023-06-04 13:16:13 -0300

    alterado modelos de acordo com exercício

--- backend/src/models/Course.js
+++ backend/src/models/Course.js
@@ -1,14 +1,18 @@
 const Joi = require("joi");
 
-const Student = require("./Course")
-
 const Course = Joi.object({
     uuid: Joi.string().guid({ version: "uuidv4" }),
     description: Joi.string().required(),
-    teacher: Joi.string().required(),
-    length: Joi.number().integer().required(),
+    courseHours: Joi.number().integer().required(),
+    startDate: Joi.date().required(),
+    finishDate: Joi.date().required(),
     subscribersNumber: Joi.number().integer().default(0),
-    students: Joi.array().items().default([])
+    students: Joi.array().items(
+        Joi.object({
+            uuid: Joi.string().guid({ version: "uuidv4" }).required(),
+            name: Joi.string().required()
+        })
+    ).default([])
 });
 
 module.exports = Course;
--- backend/src/models/Student.js
+++ backend/src/models/Student.js
@@ -3,11 +3,18 @@ const Joi = require("joi");
 const Course = require("./Course")
 
 const Student = Joi.object({
-    uuid: Joi.string().guid({version: "uuidv4"}),
+    uuid: Joi.string().guid({ version: "uuidv4" }),
     name: Joi.string().required(),
     email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
     birthDate: Joi.date().required(),
-    courses: Joi.array().items().default([])
+    courses: Joi.array().items(
+        Joi.object({
+            uuid: Joi.string().guid({ version: "uuidv4" }),
+            description: Joi.string().required(),
+            finishDate: Joi.date().required()
+        })
+    ).default([])
 });
 
 module.exports = Student;
