f22a9da016d3e824a50f4e9ebe13a95bf9036195
Thiago Elias <thiagoelias99@gmail.com>
2023-06-16 17:58:41 -0300

    adicionado swagger

--- backend/src/server/routes/welcome.js
+++ backend/src/server/routes/welcome.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const { StatusCodes } = require("http-status-codes");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.status(StatusCodes.OK).json({
-        mensagem: "Olá, esta API foi desenvolvida por Thiago Elias para o projeto do curso Back-end com NodeJS & SQL da Infnet",
-        message: "Hello! This API was developed by Thiago Elias for the project of the Back-end with NodeJS & SQL course at Infnet."
-    });
-});
+// router.get("/", (req, res) => {
+//     res.status(StatusCodes.OK).json({
+//         mensagem: "Olá, esta API foi desenvolvida por Thiago Elias para o projeto do curso Back-end com NodeJS & SQL da Infnet",
+//         message: "Hello! This API was developed by Thiago Elias for the project of the Back-end with NodeJS & SQL course at Infnet."
+//     });
+// });
+
+router.use('/', swaggerUi.serve);
+router.get('/', swaggerUi.setup(swaggerDocument))
 
 module.exports = router;
--- todo.md
+++ todo.md
@@ -1,1 +1,1 @@
-Add database
+Erro email
